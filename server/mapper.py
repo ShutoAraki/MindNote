6723bbcc266b55905761ac78e0c2fdac9ff56d44
jparenas <juan.p.arenas1@gmail.com>
2020-01-25 09:58:15 -0500

    Added torch to the requirements

--- server/mapper.py
+++ server/mapper.py
@@ -3,7 +3,7 @@ from MulticoreTSNE import MulticoreTSNE as TSNE
 
 class Mapper:
   def __init__(self):
-    self.tsne = TSNE(n_jobs=-1, n_iter=10000)
+    self.tsne = TSNE(n_jobs=-1, n_iter=10000, perplexity=40)
 
   def compute_map(self, vectors):
     vectors = np.array(vectors)
