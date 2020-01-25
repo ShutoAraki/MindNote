1540f4bcd229c1cad54fd50501b33b21bd7c402f
jparenas <juan.p.arenas1@gmail.com>
2020-01-25 07:54:24 -0500

    Changed model and started working in the front end

---
+++ client/src/components/MapRenderer.vue
@@ -0,0 +1,94 @@
+<script>
+import axios from 'axios';
+import { Scatter } from 'vue-chartjs';
+
+export default {
+  extends: Scatter,
+  data() {
+    return {
+      note_data: [],
+      coordinates: [],
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        legend: {
+          display: false
+        },
+        scales: {
+          xAxes: [
+            {
+              gridLines: {
+                display: false,
+                drawBorder: false
+              },
+              ticks: {
+                display: false
+              }
+            }
+          ],
+          yAxes: [
+            {
+              gridLines: {
+                display: false,
+                drawBorder: false
+              },
+              ticks: {
+                display: false
+              }
+            }
+          ]
+        },
+        tooltips: {
+          titleFontSize: 18,
+          callbacks: {
+            title: (tooltipItem) => {
+              return this.note_data[tooltipItem[0]['index']].title;
+            },
+            label() {
+              return ''
+            },
+            afterLabel: (tooltipItem) => {
+              return this.note_data[tooltipItem['index']].content;
+            }
+          }
+        },
+        onClick: (event) => {
+          this.handle_click(event);
+        }
+      }
+    };
+  },
+  async mounted() {
+    this.note_data = (await axios.get(process.env.VUE_APP_API_URL + '/api/map')).data;
+    for (let i = 0; i < this.note_data.length; i++) {
+      this.coordinates.push({ x: this.note_data[i].x, y: this.note_data[i].y });
+    }
+    let note_icon = new Image();
+    note_icon.src = 'http://simpleicon.com/wp-content/uploads/note.png';
+    note_icon.width = 50;
+    note_icon.height = 50;
+    this.renderChart(
+      {
+        datasets: [
+          {
+            data: this.coordinates,
+            backgroundColor: '#ff0000',
+            pointRadius: 8,
+            pointHoverRadius: 10,
+            pointStyle: note_icon
+          }
+        ]
+      },
+      this.options
+    );
+  },
+  methods: {
+    handle_click(event) {
+      let elements = this.$data._chart.getElementsAtEvent(event);
+      if (elements.length > 0) {
+        this.$router.push(`/notes/${this.note_data[elements[0]._index].id}`)
+      }
+    }
+  }
+};
+</script>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,22 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
+const routes = [{
     path: '/',
-    name: 'home',
-    component: Home
+    redirect: '/notes'
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/notes',
+    name: 'notes',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import( /* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/map',
+    name: 'map',
+    component: () => import( /* webpackChunkName: "map" */ '../views/Map.vue')
   }
 ]
 
